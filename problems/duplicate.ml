16e42508fe7000ffdecfa053a57cc54a8cf3efb7
Richard Degenne <richard.degenne+github@gmail.com>
2023-03-15 21:58:22 +0100

    Added Problem 15

--- problems/duplicate.ml
+++ problems/duplicate.ml
@@ -4,5 +4,19 @@
   - : string list = ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d"]
 *)
 
+(*
 let duplicate l =
   List.map (fun x -> [x; x]) l |> List.flatten
+*)
+
+(* 15. Replicate the elements of a list
+
+  # replicate ["a";"b";"c"] 3;;
+  - : string list = ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"]
+*)
+
+let replicate l n =
+  List.map (fun x -> List.init n (fun _ -> x)) l |> List.flatten
+
+(* Alternative duplicate definition *)
+let duplicate l = replicate l 2
--- problems/duplicate.mli
+++ problems/duplicate.mli
@@ -5,3 +5,11 @@
 *)
 
 val duplicate : 'a list -> 'a list
+
+(* 15. Replicate the elements of a list
+
+  # replicate ["a";"b";"c"] 3;;
+  - : string list = ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"]
+*)
+
+val replicate : 'a list -> int -> 'a list
