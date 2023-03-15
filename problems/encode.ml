19c6d322f52793ccaba9f7877c56959bcbbd71ab
Richard Degenne <richard.degenne+github@gmail.com>
2023-03-15 21:41:44 +0100

    Added Problem 11

--- problems/encode.ml
+++ problems/encode.ml
@@ -16,3 +16,25 @@ let encode l =
   match l with
   | [] -> []
   | h :: t -> aux [] (1, h) t |> List.rev
+
+(* 11. Modified run-length
+
+  # encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+  - : string rle list = [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";Many (4, "e")]
+*)
+
+type 'a node =
+| One of 'a
+| Many of int * 'a
+
+let node_encode l =
+  let rec aux acc sub_acc = function
+    | [] -> sub_acc :: acc
+    | h :: t ->
+      match sub_acc with
+      | One o -> if o = h then aux acc (Many (2, h)) t else aux (sub_acc :: acc) (One h) t
+      | Many (n, o) -> if o = h then aux acc (Many (n + 1, h)) t else aux (sub_acc :: acc) (One h) t
+  in
+  match l with
+  | [] -> []
+  | h :: t -> aux [] (One h) t |> List.rev
--- problems/encode.mli
+++ problems/encode.mli
@@ -6,3 +6,15 @@
 *)
 
 val encode : 'a list -> (int * 'a) list
+
+(* 11. Modified run-length
+
+  # encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+  - : string rle list = [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";Many (4, "e")]
+*)
+
+type 'a node =
+  | One of 'a
+  | Many of int * 'a
+
+val node_encode : 'a list -> 'a node list
